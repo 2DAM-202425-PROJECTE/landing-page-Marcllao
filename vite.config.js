73006ab69a63dd5423dbec0358c8baa049fbf5d0
Marc <mllao@iesebre.com>
2024-09-30 20:21:37 +0200

    prova

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 export default {
     base: '/landing-page-MarcLlao/',
-    build: {
-        rollupOptions: {
-            output: {
-                assetFileNames: (assetInfo) => {
-                    if (assetInfo.name == "style.css") return "css/styles.css";
-                    return assetInfo.name;
-                },
-            },
-        },
-    },
+    // build: {
+    //     rollupOptions: {
+    //         output: {
+    //             assetFileNames: (assetInfo) => {
+    //                 if (assetInfo.name == "style.css") return "css/styles.css";
+    //                 return assetInfo.name;
+    //             },
+    //         },
+    //     },
+    // },
 };
